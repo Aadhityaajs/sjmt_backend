a95efd5492dcba230b692ec2b82ed0f500ff9827
Aadhityaajs <aadhityaajs.projects@gmail.com>
2026-01-15 11:44:58 +0530

    first commit

---
+++ src/main/java/com/sjmt/SJMT/SjmtApplication.java
@@ -0,0 +1,13 @@
+package com.sjmt.SJMT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SjmtApplication.class, args);
+	}
+
+}
